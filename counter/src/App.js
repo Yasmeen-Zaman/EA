275355e123bc75bb575f9ab97e47d2f7763407e1
Yasmeen-Zaman <62749759+Yasmeen-Zaman@users.noreply.github.com>
2021-04-13 09:51:18 +0500

    Create App.js

---
+++ counter/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Products from "./components/Products";
+import List from "./components/PurchasedItems";
+import DataProvider from "./components/DataProvider";
+
+
+// function withData(Component) {
+//   return (props) => (
+//       <DataProvider {...props}
+//           render={(data) => {
+//               return <Component data={data} />
+//           }}
+//       ></DataProvider>
+//   );
+// }
+
+function App() {
+  // const Product = withData(Products);
+  return (
+    <div className="App">
+      <Products />
+    </div>
+  );
+}
+
+export default App;
